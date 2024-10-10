11b4df012c0554b4f09fb99f8e634e2d168be15b
babasoy84 <babasoy.ja84@gmail.com>
2024-10-10 22:32:23 +0400

    DbContext was created.

---
+++ src/Api/Infrastructure/HomeAz.Api.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HomeAz.Api.Domain.Concretes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAz.Api.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Property> Properties { get; set; }
+
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Api/Infrastructure/HomeAz.Api.Persistence/Migrations/20241010183014_init.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeAz.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrls = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    NumberOfBathrooms = table.Column<short>(type: "smallint", nullable: false),
+                    NumberOfBedrooms = table.Column<short>(type: "smallint", nullable: false),
+                    SquareMeters = table.Column<float>(type: "real", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Properties");
+        }
+    }
+}
